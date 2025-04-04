d8fed573c953847c938807c981bd737314320484
cqin626 <qinhoh@gmail.com>
2025-04-05 02:19:18 +0800

    Initialize Menu

--- app/src/main/java/my/edu/utar/mathapp/MainActivity.java
+++ app/src/main/java/my/edu/utar/mathapp/MainActivity.java
@@ -2,7 +2,13 @@ package my.edu.utar.mathapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.LocaleList;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up listener for locale toggle
+        Button toggleLocaleBtn = findViewById(R.id.button_toggle_locale);
+        toggleLocaleBtn.setOnClickListener(this::toggleLocale);
     }
+
+    private void toggleLocale(View view) {
+        Configuration configuration = new Configuration();
+        Locale currentLocale = getResources().getConfiguration().getLocales().get(0);
+
+        Locale newLocale = currentLocale.getLanguage().equals(Locale.ENGLISH.getLanguage())
+                ? Locale.SIMPLIFIED_CHINESE
+                : Locale.ENGLISH;
+
+        Locale.setDefault(newLocale);
+        configuration.setLocales(new LocaleList(newLocale));
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+        recreate();
+    }
+
 }
