dda69b5c9205e5b3287e4602404e53cca8cd91dc
cqin626 <qinhoh@gmail.com>
2025-04-07 04:48:06 +0800

    Add support to reset high scores

--- app/src/main/java/my/edu/utar/mathapp/view/ViewScoresActivity.java
+++ app/src/main/java/my/edu/utar/mathapp/view/ViewScoresActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import info.hoang8f.widget.FButton;
 import my.edu.utar.mathapp.MyApplication;
 import my.edu.utar.mathapp.R;
 import my.edu.utar.mathapp.data.SharedPreferencesDataSource;
@@ -16,6 +17,7 @@ public class ViewScoresActivity extends BaseActivity {
     private TextView composeNumVal;
 
     private SharedPreferencesDataSource dataSource;
+    private FButton deleteScoreBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +30,26 @@ public class ViewScoresActivity extends BaseActivity {
         compareNumVal = findViewById(R.id.score_compare_num);
         orderNumVal = findViewById(R.id.score_order_num);
         composeNumVal = findViewById(R.id.score_compose_num);
+        deleteScoreBtn = findViewById(R.id.score_delete_btn);
 
         setupToolbarWithBackButton();
         loadHighScores();
+        initDeleteScoreBtn();
     }
 
     private void loadHighScores() {
         compareNumVal.setText(String.valueOf(dataSource.getInt("compare_num", 0)));
         orderNumVal.setText(String.valueOf(dataSource.getInt("order_num", 0)));
         composeNumVal.setText(String.valueOf(dataSource.getInt("compose_num", 0)));
+    }
+
+    private void initDeleteScoreBtn() {
+        applyRetryButtonStyle(deleteScoreBtn);
+        deleteScoreBtn.setOnClickListener(v -> {
+            dataSource.saveInt("compare_num", 0);
+            dataSource.saveInt("order_num", 0);
+            dataSource.saveInt("compose_num", 0);
+            loadHighScores();
+        });
     }
 }
