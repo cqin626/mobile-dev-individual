2d7ee2966b0ccc830e20571abb5d46d872b28783
cqin626 <qinhoh@gmail.com>
2025-04-06 23:52:58 +0800

    Add feature to support scoring in compare numbers module

--- app/src/main/java/my/edu/utar/mathapp/view/BaseActivity.java
+++ app/src/main/java/my/edu/utar/mathapp/view/BaseActivity.java
@@ -2,24 +2,33 @@ package my.edu.utar.mathapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import info.hoang8f.widget.FButton;
+import my.edu.utar.mathapp.MyApplication;
 import my.edu.utar.mathapp.R;
+import my.edu.utar.mathapp.data.SharedPreferencesDataSource;
 
 public class BaseActivity extends AppCompatActivity {
     protected Toast currentToast;
     protected Toolbar toolbar;
+    protected SharedPreferencesDataSource dataSource;
+    protected int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Content view is set by child activity
         super.onCreate(savedInstanceState);
+        dataSource = ((MyApplication) getApplication()).getSharedPreferencesDataSource();
     }
+
     protected void showToast(String toastString) {
         if (currentToast != null) {
             currentToast.cancel();
@@ -27,6 +36,7 @@ public class BaseActivity extends AppCompatActivity {
         currentToast = Toast.makeText(getApplicationContext(), toastString, Toast.LENGTH_SHORT);
         currentToast.show();
     }
+
     protected void setupToolbarWithBackButton() {
         toolbar = findViewById(R.id.toolbar);
         if (toolbar != null) {
@@ -43,6 +53,7 @@ public class BaseActivity extends AppCompatActivity {
             }
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -50,5 +61,19 @@ public class BaseActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void incrementScore(int by, String module) {
+        score += by;
+        dataSource.saveInt(module, score);
+    }
+
+    protected void applyRetryButtonStyle(FButton btn) {
+        final Typeface customFont = ResourcesCompat.getFont(this, R.font.noto_sans_semi_bold);
+
+        btn.setTypeface(customFont);
+        btn.setButtonColor(0xFFEE4B2B);
+        btn.setShadowHeight(32);
+        btn.setCornerRadius(32);
     }
 }
--- app/src/main/java/my/edu/utar/mathapp/view/CompareNumActivity.java
+++ app/src/main/java/my/edu/utar/mathapp/view/CompareNumActivity.java
@@ -1,20 +1,27 @@
 package my.edu.utar.mathapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
+import info.hoang8f.widget.FButton;
 import my.edu.utar.mathapp.R;
 
 public class CompareNumActivity extends BaseActivity {
     private Button leftBtn;
     private Button rightBtn;
+    private FButton retryBtn;
     private TextView instruction;
+    private TextView scoreLabel;
     private boolean isPlayingGreater = false;
 
     @Override
@@ -27,15 +34,19 @@ public class CompareNumActivity extends BaseActivity {
 
         leftBtn = findViewById(R.id.compare_left_num);
         rightBtn = findViewById(R.id.compare_right_num);
+        retryBtn = findViewById(R.id.retry_btn);
         instruction = findViewById(R.id.compare_instruction);
+        scoreLabel = findViewById(R.id.score_value);
 
         reloadGame();
         initNumButtonListener(leftBtn, rightBtn);
         initNumButtonListener(rightBtn, leftBtn);
+        initRetryBtn();
     }
 
     private void reloadGame() {
         initGameMode();
+        scoreLabel.setText(String.valueOf(score));
         updateButtonsWithRandomNumbers();
     }
 
@@ -48,8 +59,14 @@ public class CompareNumActivity extends BaseActivity {
 
     private void updateButtonsWithRandomNumbers() {
         Random random = new Random();
+        boolean found = false;
         int leftNum = random.nextInt(1000);
-        int rightNum = random.nextInt(1000);
+        int rightNum = 0;
+
+        while (!found) {
+            rightNum = random.nextInt(1000);
+            found = rightNum != leftNum;
+        }
 
         leftBtn.setText(String.valueOf(leftNum));
         rightBtn.setText(String.valueOf(rightNum));
@@ -64,17 +81,43 @@ public class CompareNumActivity extends BaseActivity {
                 int currentButtonVal = Integer.parseInt(currentButton.getText().toString());
                 int oppositeButtonVal = Integer.parseInt(oppositeButton.getText().toString());
                 String feedback;
+                boolean isCorrectAnswer = isCorrect(currentButtonVal, oppositeButtonVal);
 
-                if (isPlayingGreater) {
-                    feedback = currentButtonVal > oppositeButtonVal ? correctFeedback : wrongFeedback;
-                } else {
-                    feedback = currentButtonVal < oppositeButtonVal ? correctFeedback : wrongFeedback;
-                }
+                feedback = isCorrectAnswer ? correctFeedback : wrongFeedback;
                 showToast(feedback);
-                reloadGame();
+                if (isCorrectAnswer) {
+                    incrementScore(1, "compare_num");
+                    reloadGame();
+                } else {
+                    leftBtn.setEnabled(false);
+                    rightBtn.setEnabled(false);
+                    retryBtn.setVisibility(View.VISIBLE);
+                }
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
+        });
+    }
+
+    private boolean isCorrect(int selectedNum, int oppositeNum) {
+        return isPlayingGreater
+                ? (selectedNum > oppositeNum)
+                : (selectedNum < oppositeNum);
+    }
+
+    private void initRetryBtn() {
+        applyRetryButtonStyle(retryBtn);
+        setupOnClickListenerForRetryBtn();
+    }
+
+    private void setupOnClickListenerForRetryBtn() {
+        retryBtn.setOnClickListener(v -> {
+            score = 0;
+            scoreLabel.setText(String.valueOf(score));
+            leftBtn.setEnabled(true);
+            rightBtn.setEnabled(true);
+            retryBtn.setVisibility(View.GONE);
+            reloadGame();
         });
     }
 }
