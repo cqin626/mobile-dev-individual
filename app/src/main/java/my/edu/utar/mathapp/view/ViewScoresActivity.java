0a205e08355c060869a6a336d96d16a8dcf9074e
cqin626 <qinhoh@gmail.com>
2025-04-07 03:27:37 +0800

    Add feature to support view module high scores

--- app/src/main/java/my/edu/utar/mathapp/view/ViewScoresActivity.java
+++ app/src/main/java/my/edu/utar/mathapp/view/ViewScoresActivity.java
@@ -3,17 +3,39 @@ package my.edu.utar.mathapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import my.edu.utar.mathapp.MyApplication;
 import my.edu.utar.mathapp.R;
+import my.edu.utar.mathapp.data.SharedPreferencesDataSource;
 
 public class ViewScoresActivity extends BaseActivity {
+
+    private TextView compareNumVal;
+    private TextView orderNumVal;
+    private TextView composeNumVal;
+
+    private SharedPreferencesDataSource dataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_scores);
 
+        dataSource = ((MyApplication) getApplication()).getSharedPreferencesDataSource();
+
         toolbar = findViewById(R.id.toolbar);
+        compareNumVal = findViewById(R.id.score_compare_num);
+        orderNumVal = findViewById(R.id.score_order_num);
+        composeNumVal = findViewById(R.id.score_compose_num);
+
         setupToolbarWithBackButton();
+        loadHighScores();
+    }
+
+    private void loadHighScores() {
+        compareNumVal.setText(String.valueOf(dataSource.getInt("compare_num", 0)));
+        orderNumVal.setText(String.valueOf(dataSource.getInt("order_num", 0)));
+        composeNumVal.setText(String.valueOf(dataSource.getInt("compose_num", 0)));
     }
 }
